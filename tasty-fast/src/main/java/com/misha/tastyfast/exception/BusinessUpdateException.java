0f5a848a0be0be247c81884e4cba9ce3a0739295
Mishar025 <mishamay583@gmail.com>
2024-07-23 18:53:02 -0400

    hhht

---
+++ tasty-fast/src/main/java/com/misha/tastyfast/exception/BusinessUpdateException.java
@@ -0,0 +1,7 @@
+package com.misha.tastyfast.exception;
+
+public class BusinessUpdateException extends RuntimeException {
+    public BusinessUpdateException(String msg) {
+        super(msg);
+    }
+}
