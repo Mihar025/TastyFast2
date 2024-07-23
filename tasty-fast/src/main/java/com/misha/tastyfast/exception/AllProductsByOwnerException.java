e4746c95683ab8067c66d4887e8c22620f2c6124
Mishar025 <mishamay583@gmail.com>
2024-07-23 18:49:03 -0400

    hhht

---
+++ tasty-fast/src/main/java/com/misha/tastyfast/exception/AllProductsByOwnerException.java
@@ -0,0 +1,7 @@
+package com.misha.tastyfast.exception;
+
+public class AllProductsByOwnerException extends RuntimeException {
+    public AllProductsByOwnerException(String msg) {
+        super(msg);
+    }
+}
---
+++ tasty-fast/src/main/java/com/misha/tastyfast/exception/AllProductsException.java
@@ -0,0 +1,7 @@
+package com.misha.tastyfast.exception;
+
+public class AllProductsException extends RuntimeException {
+    public AllProductsException(String msg) {
+        super(msg);
+    }
+}
