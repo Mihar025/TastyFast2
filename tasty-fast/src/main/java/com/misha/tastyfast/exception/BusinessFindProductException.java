27c24c9e71575fa4748d4b7685ed2547253c0d8f
Mishar025 <mishamay583@gmail.com>
2024-07-23 18:51:03 -0400

    hhht

---
+++ tasty-fast/src/main/java/com/misha/tastyfast/exception/BusinessFindProductException.java
@@ -0,0 +1,7 @@
+package com.misha.tastyfast.exception;
+
+public class BusinessFindProductException extends RuntimeException {
+    public BusinessFindProductException(String msg) {
+        super(msg);
+    }
+}
