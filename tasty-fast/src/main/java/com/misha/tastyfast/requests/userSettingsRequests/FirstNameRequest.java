ca32f1227e0d21f9cd6ff8f1e1843c2de05f015c
Mishar025 <mishamay583@gmail.com>
2024-07-27 15:25:28 -0400

    hhht

--- tasty-fast/src/main/java/com/misha/tastyfast/requests/userSettingsRequests/FirstNameRequest.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/requests/userSettingsRequests/FirstNameRequest.java
@@ -1,10 +1,20 @@
 package com.misha.tastyfast.requests.userSettingsRequests;
 
+import lombok.*;
+
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
 public class FirstNameRequest {
 
     private String firstName;
 
+    // Публичный конструктор с параметрами для использования билдера
+    public FirstNameRequest(String firstName) {
+        this.firstName = firstName;
+    }
 }
