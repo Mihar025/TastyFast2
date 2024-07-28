59bb5c2aee3e629714a5cafbab87402051de2bb5
Mishar025 <mishamay583@gmail.com>
2024-07-28 15:51:30 -0400

    hhht

--- tasty-fast-ui/src/app/services/fn/cart-controller/add-item-to-cart.ts
+++ tasty-fast-ui/src/app/services/fn/cart-controller/add-item-to-cart.ts
@@ -7,13 +7,13 @@ import { StrictHttpResponse } from '../../strict-http-response';
 import { RequestBuilder } from '../../request-builder';
 
 import { CartItemRequest } from '../../models/cart-item-request';
-import { CartResponse } from '../../models/cart-response';
 
 export interface AddItemToCart$Params {
       body: CartItemRequest
 }
 
-export function addItemToCart(http: HttpClient, rootUrl: string, params: AddItemToCart$Params, context?: HttpContext): Observable<StrictHttpResponse<CartResponse>> {
+export function addItemToCart(http: HttpClient, rootUrl: string, params: AddItemToCart$Params, context?: HttpContext): Observable<StrictHttpResponse<{
+}>> {
   const rb = new RequestBuilder(rootUrl, addItemToCart.PATH, 'post');
   if (params) {
     rb.body(params.body, 'application/json');
@@ -24,9 +24,10 @@ export function addItemToCart(http: HttpClient, rootUrl: string, params: AddItem
   ).pipe(
     filter((r: any): r is HttpResponse<any> => r instanceof HttpResponse),
     map((r: HttpResponse<any>) => {
-      return r as StrictHttpResponse<CartResponse>;
+      return r as StrictHttpResponse<{
+      }>;
     })
   );
 }
 
-addItemToCart.PATH = '/items';
+addItemToCart.PATH = '/cart/items';
