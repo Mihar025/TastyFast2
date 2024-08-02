d62a42a5f544a57105a49bab4746852262e007f2
Mishar025 <mishamay583@gmail.com>
2024-08-01 21:02:18 -0400

    hhht

--- tasty-fast/src/main/java/com/misha/tastyfast/feedback/req/FeedBackResponse.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/feedback/req/FeedBackResponse.java
@@ -8,7 +8,8 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class FeedBackResponse {
-
+    private Integer id;
+    private Integer businessId;
     private Double note;
     private String comment;
     private boolean ownFeedback;
