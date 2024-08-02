443b2fede198f17a5f9e9e1022b04171ef0de778
Mishar025 <mishamay583@gmail.com>
2024-08-01 21:01:43 -0400

    hhht

--- tasty-fast/src/main/java/com/misha/tastyfast/feedback/FeedbackController/FeedbackController.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/feedback/FeedbackController/FeedbackController.java
@@ -19,14 +19,56 @@ public class FeedbackController {
 
     private final FeedbackService service;
 
-    @PostMapping
-    public ResponseEntity<Integer> saveFeedBack(
+    @PostMapping("/restaurant-feedback/{restaurantId}")
+    public ResponseEntity<FeedBackResponse> saveFeedBackForRestaurant(
             @Valid @RequestBody FeedBackRequest request,
+            @PathVariable("restaurantId") Integer restaurantId,
             Authentication connectedUser
     ){
-        return ResponseEntity.ok(service.save(request, connectedUser));
+        return ResponseEntity.ok(service.saveFeedbackForRestaurant(request, restaurantId, connectedUser));
     }
 
+    @PostMapping("/store-feedback/{storeId}")
+    public ResponseEntity<FeedBackResponse> saveFeedBackForStore(
+            @Valid @RequestBody FeedBackRequest request,
+            @PathVariable("storeId") Integer storeId,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(service.saveFeedbackForStore(request, storeId, connectedUser));
+    }
+
+
+    @GetMapping("/restaurant/{restaurantId}")
+    public ResponseEntity<PageResponse<FeedBackResponse>> findAllFeedBacksByRestaurant(
+            @PathVariable("restaurantId") Integer restaurantId,
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(service.findAllRestaurantFeedbacks(restaurantId, page, size, connectedUser));
+    }
+
+    @GetMapping("/store/{storeId}")
+    public ResponseEntity<PageResponse<FeedBackResponse>> findAllFeedBacksByStore(
+            @PathVariable("storeId") Integer storeId,
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(service.findAllStoreFeedbacks(storeId, page, size, connectedUser));
+    }
+
+    @DeleteMapping("/{feedbackId}")
+    public ResponseEntity<Void> deleteFeedback(
+            @PathVariable("feedbackId") Integer feedbackId,
+            Authentication connectedUser
+    ) {
+        service.deleteFeedBackById(feedbackId, connectedUser);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    /*
     @GetMapping("/product/{productId}")
     public ResponseEntity<PageResponse<FeedBackResponse>> findAllFeedBacksByProduct(
             @PathVariable("productId") Integer productId,
@@ -57,33 +99,8 @@ public class FeedbackController {
         return ResponseEntity.ok(service.findAllFeedBacksByDrink(drinkId, page, size, connectedUser));
     }
 
-    @GetMapping("/restaurant/{restaurantId}")
-    public ResponseEntity<PageResponse<FeedBackResponse>> findAllFeedBacksByRestaurant(
-            @PathVariable("restaurantId") Integer restaurantId,
-            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
-            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
-            Authentication connectedUser
-    ){
-        return ResponseEntity.ok(service.findAllFeedBacksByRestaurant(restaurantId, page, size, connectedUser));
-    }
 
-    @GetMapping("/store/{storeId}")
-    public ResponseEntity<PageResponse<FeedBackResponse>> findAllFeedBacksByStore(
-            @PathVariable("storeId") Integer storeId,
-            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
-            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
-            Authentication connectedUser
-    ){
-        return ResponseEntity.ok(service.findAllFeedBacksByStore(storeId, page, size, connectedUser));
-    }
-
-    @DeleteMapping("/{businessId}")
-    public void deleteFeedback(
-            @PathVariable("businessId") Integer businessId,
-            Authentication connectedUser
-    ) throws Exception {
-        service.deleteFeedBackById(businessId, connectedUser);
-    }
+     */
 
 
 
