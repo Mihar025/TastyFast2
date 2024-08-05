3275d548c85add4f576785ed9d96898bf871c323
Mishar025 <mishamay583@gmail.com>
2024-08-04 20:26:54 -0400

    hhht

--- tasty-fast/src/main/java/com/misha/tastyfast/requests/dishesRequests/DishesRequest.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/requests/dishesRequests/DishesRequest.java
@@ -7,17 +7,13 @@ import java.math.BigDecimal;
 
 public record DishesRequest(
         Integer id,
-        @NotNull(message = "100")
-        @NotEmpty(message = "100")
+
         String dishesName,
-        @NotNull(message = "101")
-        @NotEmpty(message = "101")
+
         String dishesDescription,
-        @NotNull(message = "102")
-        @NotEmpty(message = "102")
+
         BigDecimal price,
-        @NotNull(message = "103")
-        @NotEmpty(message = "103")
+
         BigDecimal calories,
 
         boolean inStock,
