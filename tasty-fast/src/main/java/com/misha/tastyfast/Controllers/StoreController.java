c5713dbcd591fab4e010627dbd655be70bb29e4a
Mishar025 <mishamay583@gmail.com>
2024-08-08 18:38:07 -0400

    hhht

--- tasty-fast/src/main/java/com/misha/tastyfast/Controllers/StoreController.java
+++ tasty-fast/src/main/java/com/misha/tastyfast/Controllers/StoreController.java
@@ -1,7 +1,10 @@
 package com.misha.tastyfast.Controllers;
 
 import com.misha.tastyfast.exception.InternalServerErrorException;
+import com.misha.tastyfast.model.Restaurant;
 import com.misha.tastyfast.model.Store;
+import com.misha.tastyfast.model.User;
+import com.misha.tastyfast.repositories.StoreRepository;
 import com.misha.tastyfast.requests.drinkRequests.DrinkRequest;
 import com.misha.tastyfast.requests.drinkRequests.DrinkRequestForStore;
 import com.misha.tastyfast.requests.drinkRequests.DrinksResponse;
@@ -11,12 +14,21 @@ import com.misha.tastyfast.requests.productRequests.ProductResponse;
 import com.misha.tastyfast.requests.storeRequests.StoreRequest;
 import com.misha.tastyfast.requests.storeRequests.StoreResponse;
 import com.misha.tastyfast.services.StoreService;
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("stores")
@@ -24,11 +36,54 @@ import org.springframework.web.bind.annotation.*;
 public class StoreController {
 
     private final StoreService storeService;
+    private final StoreRepository storeRepository;
 
-    @PostMapping
-    public ResponseEntity<Store> createStore(@RequestBody StoreRequest request, Authentication authentication) {
-        Store store = storeService.createNewStore(request, authentication);
-        return ResponseEntity.ok(store);
+    @PostMapping(value = "/store-logo/{storeId}", consumes = {"image/webp", "multipart/form-data"})
+    public ResponseEntity<?> uploadStoreLogo(
+            @PathVariable("storeId") Integer storeId,
+            @Parameter()
+            @RequestPart("file") MultipartFile file,
+            Authentication authentication
+    ){
+        storeService.uploadStoreLogo(file,authentication, storeId);
+        return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("/logo/{restaurantId}")
+    public ResponseEntity<Resource> getRestaurantLogo(@PathVariable Integer restaurantId, Authentication authentication) throws IOException {
+        User user = ((User) authentication.getPrincipal());
+
+        Store store = storeRepository.findById(restaurantId)
+                .orElseThrow(() -> new EntityNotFoundException("Restaurant not found"));
+        String logoPath = store.getLogoUrl();
+
+        if (logoPath == null || logoPath.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // Удаляем начальный './' из пути, если он есть
+        if (logoPath.startsWith("./")) {
+            logoPath = logoPath.substring(2);
+        }
+
+        Resource resource = new ClassPathResource(logoPath);
+        if (!resource.exists()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(resource);
+    }
+
+
+    @PostMapping("/create-store/{ownerId}")
+    public ResponseEntity<StoreResponse> createStore(
+                                             @RequestBody StoreRequest request,
+                                             @PathVariable("ownerId") Integer ownerId,
+                                             Authentication authentication) {
+        StoreResponse store = storeService.createNewStore(request,ownerId, authentication);
+        return ResponseEntity.status(HttpStatus.CREATED).body(store);
     }
 
     @GetMapping("/{storeId}")
@@ -83,6 +138,16 @@ public class StoreController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/business-store-owner/{ownerId}")
+    public ResponseEntity<PageResponse<StoreResponse>> getAllStoresByOwner(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @PathVariable("ownerId") Integer ownerId,
+            Authentication authentication) {
+        PageResponse<StoreResponse> response = storeService.findAllStoresByBusinessOwner(page, size, ownerId,authentication);
+        return ResponseEntity.ok(response);
+    }
+
     @GetMapping("/no-delivery")
     public ResponseEntity<PageResponse<StoreResponse>> getAllStoresWithoutDelivery(
             @RequestParam(defaultValue = "0") int page,
@@ -93,24 +158,18 @@ public class StoreController {
     }
 
     @PutMapping("/{storeId}")
-    @PreAuthorize("hasRole('BUSINESS_OWNER')")
-    public ResponseEntity<Store> updateStore(
+  //  @PreAuthorize("hasRole('BUSINESS_OWNER')")
+    public ResponseEntity<StoreResponse> updateStore(
             @PathVariable Integer storeId,
             @RequestBody StoreRequest updateRequest,
             Authentication authentication) {
-        Store updatedStore = storeService.updateStore(storeId, updateRequest, authentication);
-        return ResponseEntity.ok(updatedStore);
+         var store = storeService.updateStore(storeId, updateRequest, authentication);
+        return ResponseEntity.ok(store);
     }
 
-    @DeleteMapping("/{storeId}")
-    @PreAuthorize("hasRole('BUSINESS_OWNER')")
-    public ResponseEntity<Void> deleteStore(@PathVariable Integer storeId, Authentication authentication) {
-        storeService.deleteStore(storeId, authentication);
-        return ResponseEntity.noContent().build();
-    }
 
     @PostMapping("/{storeId}/products")
-    @PreAuthorize("hasRole('BUSINESS_OWNER')")
+  //  @PreAuthorize("hasRole('BUSINESS_OWNER')")
     public ResponseEntity<ProductResponse> addProductToStore(
             @PathVariable Integer storeId,
             @RequestBody ProductRequest request,
@@ -120,7 +179,7 @@ public class StoreController {
     }
 
     @PostMapping("/{storeId}/drinks")
-    @PreAuthorize("hasRole('BUSINESS_OWNER')")
+   // @PreAuthorize("hasRole('BUSINESS_OWNER')")
     public ResponseEntity<DrinksResponse> addDrinkToStore(
             @PathVariable Integer storeId,
             @RequestBody DrinkRequestForStore request,
@@ -130,7 +189,7 @@ public class StoreController {
     }
 
     @PutMapping("/{storeId}/drinks/{drinkId}")
-    @PreAuthorize("hasRole('BUSINESS_OWNER')")
+   // @PreAuthorize("hasRole('BUSINESS_OWNER')")
     public ResponseEntity<DrinksResponse> updateDrinkInStore(
             @PathVariable Integer storeId,
             @PathVariable Integer drinkId,
@@ -140,7 +199,7 @@ public class StoreController {
         return ResponseEntity.ok(updatedDrink);
     }
     @PutMapping("/{storeId}/products/{productId}")
-    @PreAuthorize("hasRole('BUSINESS_OWNER')")
+  //  @PreAuthorize("hasRole('BUSINESS_OWNER')")
     public ResponseEntity<ProductResponse> updateProductInStore(
             @PathVariable Integer storeId,
             @PathVariable Integer productId,
@@ -151,7 +210,7 @@ public class StoreController {
     }
 
     @DeleteMapping("/{storeId}/drinks/{drinkId}")
-    @PreAuthorize("hasRole('BUSINESS_OWNER')")
+    //@PreAuthorize("hasRole('BUSINESS_OWNER')")
     public ResponseEntity<Void> deleteDrinkFromStore(
             @PathVariable Integer storeId,
             @PathVariable Integer drinkId,
@@ -161,7 +220,7 @@ public class StoreController {
     }
 
     @DeleteMapping("/{storeId}/products/{productId}")
-    @PreAuthorize("hasRole('BUSINESS_OWNER')")
+   // @PreAuthorize("hasRole('BUSINESS_OWNER')")
     public ResponseEntity<Void> deleteProductFromStore(
             @PathVariable Integer storeId,
             @PathVariable Integer productId,
@@ -170,8 +229,20 @@ public class StoreController {
         return ResponseEntity.noContent().build();
     }
 
+
+    @DeleteMapping("/{storeId}")
+    // @PreAuthorize("hasRole('BUSINESS_OWNER')")
+    public ResponseEntity<Void> deleteStore(@PathVariable Integer storeId, Authentication authentication) {
+        storeService.deleteStore(storeId, authentication);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
     @GetMapping("/{storeId}/all-info")
-    @PreAuthorize("hasRole('BUSINESS_OWNER')")
+  //  @PreAuthorize("hasRole('BUSINESS_OWNER')")
     public ResponseEntity<StoreResponse> getAllStoreInformation(@PathVariable Integer storeId, Authentication authentication) {
         StoreResponse response = storeService.findAllStoresInformation(storeId, authentication);
         return ResponseEntity.ok(response);
